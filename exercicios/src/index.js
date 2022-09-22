0454a77ca29fef5cd20c81ec338ffa01d9f8eabc
Clayton Acerbi Goncalves <acerbi10@hotmail.com>
2022-09-22 06:41:42 +0100

    Pai e Filho

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
 <div>
-    <Saudacao tipo="Bom dia " nome="Clayton" />
+    <Pai nome="Clayton" sobrenome="Acerbi">
+        <Filho nome="Lucas" sobrenome="Acerbi" />
+        <Filho nome="Gustavo" sobrenome="Acerbi" />
+        <Filho nome="Elizabete" sobrenome="Acerbi" />
+
+    </Pai>
 </div>
 ,document.getElementById('root'))
  
